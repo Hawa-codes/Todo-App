fd05d22e5819954b6f007fd8ca8c5333089b3dc1
Hawa-codes <adediranhawa@gmail.com>
2026-05-03 18:04:33 +0100

    function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
+  const [tasks, setTasks] = useState([]);
+  const [newTodos, setNewTodos] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewTodos(tasks.push(newTodos));
+    setNewTodos("");
+  };
+
   return (
     <>
-      <p>My Todo App</p>
+      <section className='container'>
+        <div className='heading'>
+          <h1 className='title'>My To-Do List</h1>
+          <form className='input' onSubmit={handleSubmit}>
+            <input type="text" placeholder='New task' value={newTodos} 
+            onChange={(e) => setNewTodos(e.target.value)} />
+            <button type='submit'>Add</button>
+          </form>  
+        </div> 
+        <div className='task'>
+          {tasks.map((task) => (
+          <div className='list'>
+            <span>{task}</span>
+            <button>delete</button>
+          </div>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
